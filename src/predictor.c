5256dad50be33aa8b5afa50fb742113bd430fb80
BlueGold <georgezhang0608@gmail.com>
2022-11-29 23:50:37 +0800

    finish gshare

--- src/predictor.c
+++ src/predictor.c
@@ -29,6 +29,15 @@ int pcIndexBits;  // Number of bits used for PC index
 int bpType;       // Branch Prediction Type
 int verbose;
 
+uint8_t taken_or_not_taken(uint8_t symbol)
+{
+    if (symbol == WT || symbol == ST) {
+        return TAKEN;
+    } else {
+        return NOTTAKEN;
+    }
+}
+
 //------------------------------------//
 //      Predictor Data Structures     //
 //------------------------------------//
@@ -42,6 +51,63 @@ int verbose;
 //        Predictor Functions         //
 //------------------------------------//
 
+//------------------------------------//
+//              GShare                //
+//------------------------------------//
+
+/*
+    Gshare Data Structures
+*/
+uint8_t* gShareTable;       // size = 2^ghistoryBits
+uint32_t gShareHistoryReg;
+uint32_t gShareMask;
+
+/*
+    Gshare Init
+*/
+void init_gshare_predictor()
+{
+    uint32_t tableSize = 1 << ghistoryBits;
+    gShareTable = (uint8_t*) malloc(tableSize * sizeof(uint8_t));
+    gShareHistoryReg = 0;
+    gShareMask = tableSize - 1;
+    for (int i = 0; i < tableSize; i++) {
+        gShareTable[i] = WN;
+    }
+}
+
+/*
+    Gshare Make
+*/
+uint8_t
+make_gshare_prediction(uint32_t pc)
+{
+    uint32_t index = (pc ^ gShareHistoryReg) & gShareMask;
+    if (taken_or_not_taken(gShareTable[index])) {
+        return TAKEN;
+    } else {
+        return NOTTAKEN;
+    }
+}
+
+/*
+    Gshare Train
+*/
+void
+train_gshare_predictor(uint32_t pc, uint8_t outcome)
+{
+    uint32_t index = (pc ^ gShareHistoryReg) & gShareMask;
+    gShareHistoryReg = ((gShareHistoryReg << 1) ^ outcome) & gShareMask;
+    if (outcome == TAKEN && gShareTable[index] != ST) {
+        gShareTable[index]++;
+    } else if (outcome == NOTTAKEN && gShareTable[index] != SN) {
+        gShareTable[index]--;
+    }
+}
+
+/*-----------------------------------*/
+
+
 // Initialize the predictor
 //
 void
@@ -50,6 +116,17 @@ init_predictor()
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+  switch(bpType) {
+    case STATIC:
+        break;
+    case GSHARE:
+        init_gshare_predictor();
+        break;
+    case TOURNAMENT:
+    case CUSTOM:
+    default:
+      break;
+  }
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -66,10 +143,14 @@ make_prediction(uint32_t pc)
   // Make a prediction based on the bpType
   switch (bpType) {
     case STATIC:
-      return TAKEN;
+        return TAKEN;
     case GSHARE:
+        return make_gshare_prediction(pc);
+        break;
     case TOURNAMENT:
+        break;
     case CUSTOM:
+        break;
     default:
       break;
   }
@@ -88,4 +169,18 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
+    
+    switch (bpType) {
+    case STATIC:
+        break;
+    case GSHARE:
+        train_gshare_predictor(pc, outcome);
+        break;
+    case TOURNAMENT:
+        break;
+    case CUSTOM:
+        break;
+    default:
+      break;
+  }
 }
